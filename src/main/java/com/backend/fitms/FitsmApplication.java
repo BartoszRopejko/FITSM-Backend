9fc44094adc0755a452189fa3b280cfeb97339ef
Bartosz Ropejko <barrop000@utp.edu.pl>
2022-02-05 12:38:51 +0100

    Initial commit

---
+++ src/main/java/com/backend/fitms/FitsmApplication.java
@@ -0,0 +1,13 @@
+package com.backend.fitms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/fitms/FitsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.fitms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
